ddd4b9e9a1951c2f69ab1bb69d42148fc6c7e601
Sibuor007 <opolotieno851@gmail.com>
2023-09-07 06:11:15 -0700

    Adjustments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - a program that prints using puts
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
