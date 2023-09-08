2e0fb2c133c73b6c6701d524838e044f4fb85269
Sibuor007 <opolotieno851@gmail.com>
2023-09-08 16:38:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ * Description: This takes command line arguments and an exit status.
+ * Return: 0 on successful, non-zero on error.
+*/
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
