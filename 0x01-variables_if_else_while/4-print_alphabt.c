2b5b69ac1d645fe88b61cc7a0e164099d8abc0fd
Sibuor007 <opolotieno851@gmail.com>
2023-09-08 17:45:01 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ * Description: This takes command line arguments and an exit status.
+ * Return: 0 on successful, non-zero on error.
+*/
+int main(void)
+{
+	char x;
+
+	x = 'a';
+	while
+		(x <= 'z') {
+			if ((x != 'q' && x != 'e') && x <= 'z')
+				putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
