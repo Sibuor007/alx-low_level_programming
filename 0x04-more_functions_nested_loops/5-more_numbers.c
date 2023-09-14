37424a33da64768bfa93679811bb5280e3762eb0
Sibuor007 <opolotieno851@gmail.com>
2023-09-14 14:34:00 -0700

    More number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ * from 0 to 14 followed by a newline
+ * Return: 0 always
+ */
+
+void more_numbers(void)
+{
+	int z;
+
+	for (z = 0; z <=14; z++)
+	{
+	_putchar(z);
+	}
+	_putchar('\n');
+}	
