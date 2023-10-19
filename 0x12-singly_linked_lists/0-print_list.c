48101a00a97fc625b1a26c02135a37271cb84006
Sibuor007 <opolotieno851@gmail.com>
2023-10-19 13:08:17 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,14 +11,13 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 	while (h)
- 	{
-		if (h->str == NULL) {
+	{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
- 		} else {
+		else
 			printf("[%u] %s \n", h->len, h->str);
-		}
-	h = h->next;
-	count++;
+		h = h->next;
+		count++;
 	}
-	return count;
+	return (count);
 }
